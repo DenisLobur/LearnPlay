cf70b73c6c7661a7989316bd27a33d65f36f32c5
denislobur <denisdenisdenis@bigmir.net>
2020-05-14 22:18:19 +0300

    Finish video 4

--- app/controllers/TaskListBegin.scala
+++ app/controllers/TaskListBegin.scala
@@ -7,7 +7,8 @@ import play.api.i18n._
 @Singleton
 class TaskListBegin @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def taskListBegin1 = Action{
-    Ok("This works!")
+  def taskListBegin1: Action[AnyContent] = Action {
+    val tasks = List("task 1", "task 2", "task 3")
+    Ok(views.html.taskListBegin1(tasks))
   }
 }
