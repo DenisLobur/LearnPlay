b3508cb985641f86d8566039c2a5b63bf1ff6c26
denislobur <denisdenisdenis@bigmir.net>
2020-06-26 17:32:10 +0300

    Finished video 12. CSRF

--- app/controllers/TaskListBegin.scala
+++ app/controllers/TaskListBegin.scala
@@ -12,7 +12,7 @@ class TaskListBegin @Inject()(cc: ControllerComponents) extends AbstractControll
     Ok(views.html.index("check"))
   }
 
-  def login = Action {
+  def login = Action { implicit request =>
     Ok(views.html.login1())
   }
 
@@ -20,7 +20,7 @@ class TaskListBegin @Inject()(cc: ControllerComponents) extends AbstractControll
     Ok(s"User $userName was logged with the password $password")
   }
 
-  def validateLoginPost = Action { request =>
+  def validateLoginPost = Action { implicit request =>
     val requestVals = request.body.asFormUrlEncoded
     requestVals.map { args =>
       val username = args("username").head
@@ -33,7 +33,7 @@ class TaskListBegin @Inject()(cc: ControllerComponents) extends AbstractControll
     }.getOrElse(Redirect(routes.TaskListBegin.login()))
   }
 
-  def createUser = Action { request =>
+  def createUser = Action { implicit request =>
     val postVals = request.body.asFormUrlEncoded
     postVals.map { args =>
       val username = args("username").head
@@ -46,7 +46,7 @@ class TaskListBegin @Inject()(cc: ControllerComponents) extends AbstractControll
     }.getOrElse(Redirect(routes.TaskListBegin.login()))
   }
 
-  def taskListBegin1: Action[AnyContent] = Action { request =>
+  def taskListBegin1: Action[AnyContent] = Action { implicit request =>
     val usernameOption = request.session.get("username")
     usernameOption.map { username =>
       val list = TaskListInMemoryModel.getTasks(username)
