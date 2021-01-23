3c8ebfca98c2c47bbffed318e279bcc91cf6541b
denislobur <denismachtalles@gmail.com>
2021-01-23 23:28:16 +0200

    update gitignore

---
+++ client/src/main/scala/ScalaJSExample.scala
@@ -0,0 +1,19 @@
+object ScalaJSExample {
+  def main(args: Array[String]): Unit = {
+    println("This is in Scala.js.")
+//    if (dom.document.getElementById("scalajsShoutOut") != null) {
+//      dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
+//    }
+//
+//    if (dom.document.getElementById("version6") != null) {
+//      Version6.init()
+//    }
+//
+//    if (dom.document.getElementById("version7") != null) {
+//      ReactDOM.render(
+//        Version7MainComponent(),
+//        dom.document.getElementById("react-root")
+//      )
+//    }
+  }
+}
