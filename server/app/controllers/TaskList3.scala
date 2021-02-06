0bac2808060161b8fb0a74c75832ae16b664cefc
denislobur <denismachtalles@gmail.com>
2021-02-07 00:52:33 +0200

    Finished video 39. Add validate method

--- server/app/controllers/TaskList3.scala
+++ server/app/controllers/TaskList3.scala
@@ -1,17 +1,35 @@
 package controllers
 
+import com.fasterxml.jackson.databind.annotation.JacksonStdImpl
 import javax.inject._
-import models.TaskListInMemoryModel
+import models.{TaskListInMemoryModel, UserData}
 import play.api.data._
-import play.api.libs.json.Json
+import play.api.libs.json._
 import play.api.mvc._
+import play.api.i18n._
 
 @Singleton
 class TaskList3 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def
-  load = Action { implicit request =>
+  def load = Action { implicit request =>
     Ok(views.html.version3Main())
+  }
+
+  implicit val userDataReads = Json.reads[UserData]
+
+  def validate = Action { implicit request =>
+    request.body.asJson.map { body =>
+      Json.fromJson[UserData](body) match {
+        case JsSuccess(userData, _) => {
+          if (TaskListInMemoryModel.validateUser(userData.userName, userData.password)) {
+            Ok(Json.toJson(true)).withSession("username" -> userData.userName, "csrfToken" -> play.filters.csrf.CSRF.getToken)
+          } else {
+            Ok(views.html.login2())
+          }
+        }
+        case e@JsError(_) => Redirect(routes.TaskList3.load())
+      }
+    }.getOrElse(Redirect(routes.TaskList3.load()))
   }
 
   def data = Action {
---
+++ server/app/models/UserData.scala
@@ -0,0 +1,3 @@
+package models
+
+case class UserData(userName: String, password: String)
