22564a4aa06659df2d5c66732a45cc8c02ba2874
denislobur <denismachtalles@gmail.com>
2021-02-13 22:55:08 +0200

    fix build

--- server/app/controllers/TaskList3.scala
+++ server/app/controllers/TaskList3.scala
@@ -1,12 +1,9 @@
 package controllers
 
-import com.fasterxml.jackson.databind.annotation.JacksonStdImpl
 import javax.inject._
 import models.{TaskListInMemoryModel, UserData}
-import play.api.data._
 import play.api.libs.json._
 import play.api.mvc._
-import play.api.i18n._
 
 @Singleton
 class TaskList3 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
@@ -22,7 +19,7 @@ class TaskList3 @Inject()(cc: ControllerComponents) extends AbstractController(c
       Json.fromJson[UserData](body) match {
         case JsSuccess(userData, _) => {
           if (TaskListInMemoryModel.validateUser(userData.userName, userData.password)) {
-            Ok(Json.toJson(true)).withSession("username" -> userData.userName, "csrfToken" -> play.filters.csrf.CSRF.getToken)
+            Ok(Json.toJson(true)).withSession("username" -> userData.userName, "csrfToken" -> play.filters.csrf.CSRF.getToken.map(_.value).getOrElse(""))
           } else {
             Ok(views.html.login2())
           }
