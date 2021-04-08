22aa83824ae2c27b1a33e81d81798d84836a29a4
denislobur <denismachtalles@gmail.com>
2021-04-08 23:12:37 +0300

    Finished video 40. With errors of parsing JSON

---
+++ server/public/javascript/version3.js
@@ -0,0 +1,18 @@
+const csrfToken = document.getElementById("csrfToken").value
+const validateRoute = document.getElementById("validateRoute").value
+const createRoute = document.getElementById("createRoute").value
+const deleteRoute = document.getElementById("deleteRoute").value
+const addRoute = document.getElementById("addRoute").value
+
+function login() {
+    const username = document.getElementById("loginName").value
+    const password = document.getElementById("loginPass").value
+
+    fetch(validateRoute, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: {username, password}
+    }).then(res => res.json()).then(data => {
+        console.log(data)
+    })
+}
