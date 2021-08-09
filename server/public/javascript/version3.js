a0939eede7f6619dc7394f0e514cdba385794ec5
denislobur <denismachtalles@gmail.com>
2021-08-09 23:40:40 +0300

    Finished video 41

--- server/public/javascript/version3.js
+++ server/public/javascript/version3.js
@@ -1,8 +1,11 @@
+"use strict"
+
 const csrfToken = document.getElementById("csrfToken").value
 const validateRoute = document.getElementById("validateRoute").value
 const createRoute = document.getElementById("createRoute").value
 const deleteRoute = document.getElementById("deleteRoute").value
 const addRoute = document.getElementById("addRoute").value
+const tasksRoute = document.getElementById("tasksRoute").value
 
 function login() {
     const username = document.getElementById("loginName").value
@@ -10,9 +13,21 @@ function login() {
 
     fetch(validateRoute, {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: {username, password}
-    }).then(res => res.json()).then(data => {
-        console.log(data)
-    })
+        headers: {'Content-Type': 'application/json', 'Csrf-Token': csrfToken},
+        body: JSON.stringify({username, password})
+    }).then(res => res.text()).then(data => {
+        if (data) {
+            document.getElementById("login-section").hidden = true;
+            document.getElementById("task-section").hidden = false;
+            loadTasks();
+        } else {
+            //TODO:
+        }
+    });
+}
+
+function loadTasks() {
+    fetch(tasksRoute).then(res => res.json()).then(tasks => {
+
+    });
 }
